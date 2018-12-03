f34e8aa8d0590495c8ee3d5305f9092452227a8a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-03 09:05:40 -0800

    Clean up ignored files

--- server/config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI: MONGODB DATABASE URL HERE,
-  secretOrKey: SECRET HERE,
-  googleClientId:
-    GOOGLE CLIENT ID HERE,
-  googleClientSecret: GOOGLE CLIENT SECRET HERE,
-  googleCallbackURL: GOOGLE CALLBACK URL HERE EXAMPLE: "/api/users/google/redirect",
-  defaultGooglePassword: SOME DUMMY PASSWORD HERE
-};
