10555081bc15da35a66f01e2e83400b456a93861
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-05 09:20:21 -0800

    updated the formGrop to support input-group

--- src/components/ui/FormField.js
+++ src/components/ui/FormField.js
@@ -1,45 +1,117 @@
-import React from 'react';
-import classnames from 'classnames';
+import React from "react";
+import classnames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faGithub,
+  faYoutube,
+  faLinkedin,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 const FormField = ({ field, onChangeHandler }) => {
-    const generateInputField = () => {
-        let template;
-        switch (field.type) {
-            case 'select':
-                template = <select
-                    value={field.value}
-                    onChange={onChangeHandler}
-                    className={classnames('form-control form-control-lg', {
-                        'is-invalid': field.validationMessage
-                    })}
-                >
-                    {
-                        field.options.map(option => (<option value={option}>{option}</option>))
-                    }
-                </select>
-
-            default:
-                template = <input
-                    type={field.type}
-                    value={field.value}
-                    name={field.name}
-                    onChange={onChangeHandler}
-                    placeholder={field.placeholder}
-                    className={classnames('form-control form-control-lg', {
-                        'is-invalid': field.validationMessage
-                    })}
-                />
-        }
-
-        return template;
-    };
-
-    return (
-        <div className="form-group">
-            {generateInputField()}
-            {field.validationMessage && (<div className="invalid-feedback">{field.validationMessage}</div>)}
-        </div>
+  const generateFieldInstructions = message => {
+    const messageTemplate = (
+      <small className="form-text text-muted">{message}</small>
     );
+    return messageTemplate;
+  };
+  const generateInputField = () => {
+    let template;
+
+    if (field.inputGroup) {
+      let icon;
+      if (field.icon === "twitter") icon = faTwitter;
+      if (field.icon === "facebook") icon = faFacebook;
+      if (field.icon === "youtube") icon = faYoutube;
+      if (field.icon === "linkedin") icon = faLinkedin;
+      if (field.icon === "github") icon = faGithub;
+      if (field.icon === "instagram") icon = faInstagram;
+      console.log("inputGroup");
+      template = (
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">
+              <FontAwesomeIcon icon={icon} />
+            </span>
+          </div>
+          <input
+            type={field.type}
+            className="form-control form-control-lg"
+            placeholder={field.placeholder}
+            name={field.name}
+          />
+        </div>
+      );
+      return template;
+    }
+
+    if (field.type === "select") {
+      template = (
+        <select
+          value={field.value}
+          onChange={onChangeHandler}
+          className={classnames("form-control form-control-lg", {
+            "is-invalid": field.validationMessage
+          })}
+        >
+          {field.options.map(option => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      );
+    }
+
+    if (field.type === "textarea") {
+      template = (
+        <textarea
+          value={field.value}
+          name={field.name}
+          onChange={onChangeHandler}
+          placeholder={field.placeholder}
+          className={classnames("form-control form-colontrol-lg", {
+            "is-invalid": field.validationMessage
+          })}
+        />
+      );
+    }
+
+    if (
+      field.type === "text" ||
+      field.type === "email" ||
+      field.type === "number" ||
+      field.type === "password"
+    ) {
+      template = (
+        <input
+          type={field.type}
+          value={field.value}
+          name={field.name}
+          onChange={onChangeHandler}
+          placeholder={field.placeholder}
+          className={classnames("form-control form-control-lg", {
+            "is-invalid": field.validationMessage
+          })}
+        />
+      );
+    }
+    return template;
+  };
+
+  return (
+    <div className="form-group">
+      {generateInputField()}
+      {field.instructions
+        ? generateFieldInstructions(field.instructions)
+        : null}
+      {field.validationMessage && (
+        <div className="invalid-feedback">{field.validationMessage}</div>
+      )}
+    </div>
+  );
 };
 
 export default FormField;
