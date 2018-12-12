73e440f4ae123795833267c5834fab246ed3fca0
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-12 09:45:46 -0800

    orgnized the keys for security optimization

---
+++ server/config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleCallbackURL: process.env.GOOGLE_CALLBACK_URL,
+  defaultGooglePassword: process.env.DEFAULT_GOOGLE_PASSWORD
+};
